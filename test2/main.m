3e05d63333e9a93d8d2ca7bc47c25616898a3302
曹燕兵 <soutsubamehei@caoyanbingdeimac.local>
2017-04-25 10:48:38 +0800

    Initial Commit

---
+++ test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by 曹燕兵 on 2017/4/25.
+//  Copyright © 2017年 曹燕兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
